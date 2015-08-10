c46cd15929120fef21d3ab4099465d641c7241a9
dreamweaver1231 <dreamweaver1231@gmail.com>
2015-08-11 03:00:46 +0530

    added mockdata and reverted to angular 1.3

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('fullCartApp')
+angular.module('fullCartApp.admin')
   .controller('AdminCtrl', function ($scope, $http, Auth, User) {
 
     // Use the User $resource to fetch all users
--- client/app/admin/admin.js
+++ client/app/admin/admin.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('fullCartApp')
+angular.module('fullCartApp.admin')
   .config(function ($stateProvider) {
     $stateProvider
       .state('admin', {
